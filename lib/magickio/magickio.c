c297685c56674a8215c26eb6df10bb1d00bdad73
Reuben Hawkins <reubenhwk@gmail.com>
2014-05-13 06:55:28 -0500

    magickio.c: add rgb and rgba function
    The gray function now calls into the rgba function rather than doing the
    grayscale conversion and image loading in the same place...reuse the rgba
    function instead.

--- lib/magickio/magickio.c
+++ lib/magickio/magickio.c
@@ -9,9 +9,9 @@
 #include "fileio.h"
 
 /**
- * @brief read an image file into a 32bit float array, converted to gray
+ * @brief read an image file into an unsigned char array
  */
-float *read_img_f32_gray(const char *fname, size_t * nx, size_t * ny)
+unsigned char *read_img_rgb(const char *fname, size_t * nx, size_t * ny)
 {
 
 	ExceptionInfo *exception = AcquireExceptionInfo();
@@ -36,16 +36,46 @@ float *read_img_f32_gray(const char *fname, size_t * nx, size_t * ny)
 	ExportImagePixels(image, 0, 0, image->magick_columns,
 			  image->magick_rows, "RGB", CharPixel, rgb, exception);
 
-	float *retval =
-	    malloc(sizeof(float) * image->magick_columns * image->magick_rows);
-#pragma omp parallel for
-	for (size_t i = 0; i < image->magick_columns * image->magick_rows; ++i) {
-		retval[i] = (6969.0 * rgb[3 * i + 0]
-			     + 23434.0 * rgb[3 * i + 1]
-			     + 2365.0 * rgb[3 * i + 2]) / 32768.0;
+	if (exception->severity != UndefinedException)
+		CatchException(exception);
+
+	*nx = image->magick_columns;
+	*ny = image->magick_rows;
+
+	image = DestroyImage(image);
+	image_info = DestroyImageInfo(image_info);
+	exception = DestroyExceptionInfo(exception);
+
+	return rgb;
+}
+
+/**
+ * @brief read an image file into an unsigned char array
+ */
+unsigned char *read_img_rgba(const char *fname, size_t * nx, size_t * ny)
+{
+
+	ExceptionInfo *exception = AcquireExceptionInfo();
+	ImageInfo *image_info = CloneImageInfo((ImageInfo *) NULL);
+	unsigned char *file_data = 0;
+	size_t file_data_size = read_file(&file_data, fname);
+	Image *image =
+	    BlobToImage(image_info, file_data, file_data_size, exception);
+
+	if (exception->severity != UndefinedException)
+		CatchException(exception);
+	if (!image) {
+		fprintf(stderr, "failed to read image to a blob.\n");
+		exit(-1);
 	}
 
-	free(rgb);
+	if (file_data)
+		free(file_data);
+
+	unsigned char *rgba =
+	    malloc(4 * image->magick_columns * image->magick_rows);
+	ExportImagePixels(image, 0, 0, image->magick_columns,
+			  image->magick_rows, "RGBA", CharPixel, rgba, exception);
 
 	if (exception->severity != UndefinedException)
 		CatchException(exception);
@@ -57,5 +87,27 @@ float *read_img_f32_gray(const char *fname, size_t * nx, size_t * ny)
 	image_info = DestroyImageInfo(image_info);
 	exception = DestroyExceptionInfo(exception);
 
+	return rgba;
+}
+
+/**
+ * @brief read an image file into a 32bit float array, converted to gray
+ */
+float *read_img_f32_gray(const char *fname, size_t * nx, size_t * ny)
+{
+	unsigned char * rgba = read_img_rgba(fname, nx, ny);
+
+	size_t count = (*nx) * (*ny);
+	float *retval = malloc(sizeof(float) * count);
+#pragma omp parallel for
+	for (size_t i = 0; i < count; ++i) {
+		retval[i] = (6969.0 * rgba[4 * i + 0]
+			     + 23434.0 * rgba[4 * i + 1]
+			     + 2365.0 * rgba[4 * i + 2]) / 32768.0;
+	}
+
+	free(rgba);
+
 	return retval;
 }
+
--- lib/magickio/magickio.h
+++ lib/magickio/magickio.h
@@ -5,7 +5,9 @@
 extern "C" {
 #endif
 
-	float *read_img_f32_gray(const char *fname, size_t * nx, size_t * ny);
+float *read_img_f32_gray(const char *fname, size_t * nx, size_t * ny);
+unsigned char *read_img_rgb(const char *fname, size_t * nx, size_t * ny);
+unsigned char *read_img_rgba(const char *fname, size_t * nx, size_t * ny);
 
 #ifdef __cplusplus
 }
